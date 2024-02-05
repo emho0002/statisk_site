0591fcd6bb457ed20a104e2f85a86d1ed33eecc4
Emma <emho0002@stud.kea.dk>
2024-02-05 22:33:51 +0100

    prøver at få siderne til at linke op men virker ikke

--- productlist.js
+++ productlist.js
@@ -1,3 +1,6 @@
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get("category");
+
 fetch("https://kea-alt-del.dk/t7/api/products")
   .then((Response) => Response.json())
   .then(showProducts);
@@ -49,6 +52,16 @@ function showProduct(product) {
     //   produktet er udolgt
     copy.querySelector("article").classList.add("soldOut");
   }
+
+  /*   copy.querySelector("h4").textContent = product.productdisplayname;
+  if (product.soldout) {
+    //produktet er udsolgt
+    copy.querySelector("article").classList.add("soldOut");
+  }
+ */
+
+  copy.querySelector(".read_more").setAttribute("href", `product.html?id=${product.id}`);
+  copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
 
   // appende til DOM
   document.querySelector(".grid2").appendChild(copy);
